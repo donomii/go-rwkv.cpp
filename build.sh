b3576fb2cb52fccacf8ed430cb38c37dabf8b98a
Jeremy Price <donomii@gmail.com>
2023-05-02 22:51:05 +0200

    Refactor more

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-cd aimodels
-cat $(ls x*) > RWKV-4-Raven-14B-v9-Eng99%-Other1%-20230412-ctx8192_quant4.bin 
-cd ..
-
 cd rwkv.cpp
 cmake .
 cmake --build . --config Release
--- test.go
+++ test.go
@@ -9,33 +9,19 @@ import (
 	"os"
 	"strings"
 	"text/template"
-
 )
 
 // A structure to hold the conversation state
 type ConversationState struct {
 	// The context
-	ctx *Context
+	ctx      *Context
 	UserText []string
 	BotText  []string
 }
 
-
-
-
 func main() {
-
-	ctx, err := InitFromFile("aimodels/RWKV-4-Raven-1B5-v9-Eng99%-Other1%-20230411-ctx4096_quant4.bin", 8)
-
-	elem_size := ctx.GetStateBufferElementCount()
-	logit_size := ctx.GetLogitsBufferElementCount()
-
-	elem_buff := make([]float32, elem_size)
-	logit_buff := make([]float32, logit_size)
-	if err != nil {
-		panic(err)
-	}
-	preambleTemplate := `The following is a verbose detailed conversation between {{ .User }} and a woman {{ .Bot }}. {{ .Bot }} is intelligent, friendly and likeable. {{ .Bot }} is likely to agree with {{ .User }}.
+	Model := LoadFiles("aimodels/RWKV-4-Raven-1B5-v9-Eng99%-Other1%-20230411-ctx4096_quant4.bin", "rwkv.cpp/rwkv/20B_tokenizer.json", 8)
+	preambleTemplate := `The following is a verbose detailed conversation between {{ .User }} and a woman, {{ .Bot }}. {{ .Bot }} is intelligent, friendly and likeable. {{ .Bot }} is likely to agree with {{ .User }}.
 
 {{ .User }}{{ .Separator }} Hello {{ .Bot }}, how are you doing?
 
@@ -62,53 +48,33 @@ func main() {
 	//collect the template results in a buffer
 	var b bytes.Buffer
 	t := template.Must(template.New("preamble").Parse(preambleTemplate))
-	err = t.Execute(&b, preamble)
+	err := t.Execute(&b, preamble)
 	if err != nil {
 		panic(err)
 	}
 	pre := b.String()
 
-	tk, err := LoadTokeniser("rwkv.cpp/rwkv/20B_tokenizer.json")
-	if err != nil {
-		panic(err)
-	}
-	tokens, err := tk.Encode(pre)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Loading preamble")
-
-	for _, t := range tokens {
-		fmt.Print(t.Value)
-
-		elem_buff, logit_buff, _, err = ctx.Eval(int32(t.ID), elem_buff)
-		if err != nil {
-			panic(err)
-		}
-
-	}
+	Model.ProcessInput(pre)
 	var conv ConversationState
 	//if the conversation file exists, load it, print it, and processit as input, adding the correct names
-	//if it doesn't exist, create it an empty data struct
+	//if it doesn't exist, create it as an empty data struct
 	if _, err := os.Stat("conversation.json"); err == nil {
 		fmt.Println("Loading conversation")
 		data, err := ioutil.ReadFile("conversation.json")
 		if err != nil {
 			panic(err)
 		}
-		
+
 		err = json.Unmarshal(data, &conv)
 		if err != nil {
 			panic(err)
 		}
 		for i := 0; i < len(conv.UserText); i++ {
-			fmt.Println(conv.UserText[i])
-			fmt.Println(conv.BotText[i])
+			//fmt.Println(conv.UserText[i])
+			//fmt.Println(conv.BotText[i])
 			input := "\n\nBob: " + conv.UserText[i] + "\n\nAlice: " + conv.BotText[i]
-			elem_buff, logit_buff, err = process_input(input, elem_buff, &tk, ctx)
-			if err != nil {
-				panic(err)
-			}
+			Model.ProcessInput(input)
+		
 		}
 	} else {
 		fmt.Println("Creating conversation")
@@ -122,15 +88,14 @@ func main() {
 		ioutil.WriteFile("conversation.json", data, 0644)
 	}
 
-
 	// Read lines from stdin, and submit them to the model, until the user types exit
 
-	fmt.Println("Enter text to send to the model, or type exit to quit")
+	//fmt.Println("Enter text to send to the model, or type exit to quit")
 	conv = ConversationState{}
 	reader := bufio.NewReader(os.Stdin)
-	
+
 	for {
-		fmt.Print("Enter text: ")
+		fmt.Print("\n\nBob: ")
 		text, _ := reader.ReadString('\n')
 		text = strings.Replace(text, "\n", "", -1)
 		conv.UserText = append(conv.UserText, text)
@@ -140,33 +105,14 @@ func main() {
 			break
 		}
 
-		elem_buff, logit_buff, err = process_input(text, elem_buff, &tk, ctx)
-		response_text := ""
-		for i := 0; i < 100; i++ {
-			
-
-			newtoken, err := sampleLogits(logit_buff, 0.2, 1, map[int]float32{})
-			if err != nil {
-				panic(err)
-			}
-
-			elem_buff, logit_buff, _, err = ctx.Eval(int32(newtoken), elem_buff)
-			if err != nil {
-				panic(err)
-			}
-
-			chars := DeTokenise(tk, []int{newtoken})
-			fmt.Print(chars)
-			response_text += chars
-
-			if strings.Contains(chars, "\n") {
-				break
-			}
-		}
+		Model.ProcessInput(text)
+		response_text :=Model.GenerateResponse(100, "\n")
+		fmt.Print(response_text)
 
 		conv.BotText = append(conv.BotText, response_text)
+
 		//Save the conversation state to a file
-		data ,err:= json.Marshal(conv)
+		data, err := json.Marshal(conv)
 		if err != nil {
 			panic(err)
 		}
@@ -174,7 +120,6 @@ func main() {
 	}
 
 }
-
 
 func process_input(input string, elem_buff []float32, tk *Tokenizer, ctx *Context) ([]float32, []float32, error) {
 	tokens, err := tk.Encode(input)
@@ -185,7 +130,7 @@ func process_input(input string, elem_buff []float32, tk *Tokenizer, ctx *Contex
 	logit_buff := make([]float32, ctx.GetLogitsBufferElementCount())
 
 	for _, t := range tokens {
-		fmt.Print(t.Value)
+		
 
 		elem_buff, logit_buff, _, err = ctx.Eval(int32(t.ID), elem_buff)
 		if err != nil {
@@ -195,4 +140,86 @@ func process_input(input string, elem_buff []float32, tk *Tokenizer, ctx *Contex
 	}
 
 	return elem_buff, logit_buff, nil
+}
+
+type RwkvState struct {
+	// The context
+	Context   *Context
+	State     []float32
+	Logits    []float32
+	Tokenizer *Tokenizer
+}
+
+// Loadfiles("aimodels/RWKV-4-Raven-1B5-v9-Eng99%-Other1%-20230411-ctx4096_quant4.bin", "rwkv.cpp/rwkv/20B_tokenizer.json", 8)
+
+func LoadFiles(modelFile, tokenFile string, threads uint32) *RwkvState {
+	ctx, err := InitFromFile(modelFile, threads)
+
+	elem_size := ctx.GetStateBufferElementCount()
+	logit_size := ctx.GetLogitsBufferElementCount()
+
+	elem_buff := make([]float32, elem_size)
+	logit_buff := make([]float32, logit_size)
+	if err != nil {
+		return nil
+	}
+
+	tk, err := LoadTokeniser(tokenFile)
+	if err != nil {
+		return nil
+	}
+
+	return &RwkvState{
+		Context:   ctx,
+		State:     elem_buff,
+		Logits:    logit_buff,
+		Tokenizer: &tk,
+	}
+}
+
+func (r *RwkvState) ProcessInput(input string) error {
+	elems, logites, err := process_input(input, r.State, r.Tokenizer, r.Context)
+	if err != nil {
+		return err
+	}
+	r.State = elems
+	r.Logits = logites
+	return nil
+}
+
+func (r *RwkvState) PredictNextToken() string {
+	newtoken, err := sampleLogits(r.Logits, 0.2, 1, map[int]float32{})
+	if err != nil {
+		panic(err)
+	}
+
+	chars := DeTokenise(*r.Tokenizer, []int{newtoken})
+	return chars
+}
+
+func (r *RwkvState) GenerateResponse(maxTokens int, stopString string) string {
+	response_text := ""
+	for i := 0; i < maxTokens; i++ {
+
+		newtoken, err := sampleLogits(r.Logits, 0.2, 1, map[int]float32{})
+		if err != nil {
+			panic(err)
+		}
+
+		r.State, r.Logits, _, err = r.Context.Eval(int32(newtoken), r.State)
+		if err != nil {
+			panic(err)
+		}
+
+		chars := DeTokenise(*r.Tokenizer, []int{newtoken})
+		response_text += chars
+
+		if strings.Contains(response_text, stopString) {
+			//Split on stopstring, and return the first part
+			response_text = strings.Split(response_text, stopString)[0]
+			break
+		}
+	}
+	return response_text
+
 }
