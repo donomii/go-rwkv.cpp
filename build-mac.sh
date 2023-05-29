2d64e5426198b7e8860fb73535bea653a3b7cdfc
Jeremy Price <donomii@gmail.com>
2023-05-29 09:41:40 +0200

    .

--- build-mac.sh
+++ build-mac.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-export C_INCLUDE_PATH=$PWD
-export LIBRARY_PATH=$PWD 
+ 
+ cd rwkv.cpp
+cmake .
+cmake --build . --config Release
+cp librwkv.dylib ..
+ cd ..
+ 
+ cd examples
+ go build -o ai .
+ cd ..
+ cp examples/ai .
 
-cd rwkv.cpp
-cmake . -DRWKV_BUILD_SHARED_LIBRARY=OFF
-cmake --build .
-cp librwkv.a ..
-cp ggml/src/libggml.a ..
-cd ..
-
-cd examples
-go build -o ai .
-cd ..
-cp examples/ai .
+export DYLD_LIBRARY_PATH=./ 
+./ai
 cd aimodels
