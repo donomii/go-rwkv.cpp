6c59edb571c98643847a54cb87c65a49817fcfa0
Jeremy Price <donomii@gmail.com>
2023-06-04 22:24:20 +0200

    Fix downloader, add a Q&A example

--- aimodels/downloadconvert.sh
+++ aimodels/downloadconvert.sh
@@ -7,11 +7,11 @@ SMALL=RWKV-4-Raven-1B5-v12-Eng98%-Other2%-20230520-ctx4096
 
 wget -c https://huggingface.co/BlinkDL/rwkv-4-raven/resolve/main/${SMALL}.pth
 python3 ../rwkv.cpp/rwkv//convert_pytorch_to_ggml.py ${SMALL}.pth         ${SMALL}_float16.bin  float16
-python3 ../rwkv.cpp/rwkv/quantize.py                 ${SMALL}_float16.bin ${SMALL}_quant4.bin Q4_2
+python3 ../rwkv.cpp/rwkv/quantize.py                 ${SMALL}_float16.bin ${SMALL}_quant4.bin Q4_0
 cp ${SMALL}_quant4.bin small.bin
 
 wget -c https://huggingface.co/BlinkDL/rwkv-4-raven/resolve/main/${LARGE}.pth
 python3 ../rwkv.cpp/rwkv//convert_pytorch_to_ggml.py ${LARGE}.pth         ${LARGE}_float16.bin  float16
-python3 ../rwkv.cpp/rwkv/quantize.py                 ${LARGE}_float16.bin ${LARGE}_quant4.bin Q4_2
+python3 ../rwkv.cpp/rwkv/quantize.py                 ${LARGE}_float16.bin ${LARGE}_quant4.bin Q4_0
 cp ${LARGE}_quant4.bin large.bin
 
